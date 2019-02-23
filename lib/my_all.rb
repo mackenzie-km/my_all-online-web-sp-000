38e9717155b76bcdd4e6ec48dee95a11444879de
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-23 00:29:28 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,8 @@ def my_all?(collection)
   new_array = []
   i = 0 
 while i < collection.length
-i += 1 
 new_array << yield(collection[i])
+i += 1 
 new_array.include?(false)? false: true
 end
 end
